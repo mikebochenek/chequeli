6e06a778e519fe5cf30c25bbc43babca98d91413
mike.bochenek@gmail.com <mike.bochenek@gmail.com>
2020-07-20 23:16:31 +0200

    add STATIC_ROOT = os.path.join(BASE_DIR, \\static/\)

--- 01-Login/webappexample/settings.py
+++ 01-Login/webappexample/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 ENV_FILE = find_dotenv()
 if ENV_FILE:
