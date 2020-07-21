baaf5068ad009ec6061c8a3e237d3190ebc14f0a
mike.bochenek@gmail.com <mike.bochenek@gmail.com>
2020-07-21 23:36:21 +0200

    move to psql for backend

--- 01-Login/webappexample/settings.py
+++ 01-Login/webappexample/settings.py
@@ -79,8 +79,15 @@ WSGI_APPLICATION = 'webappexample.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'chequelidev',
+        'USER': 'test3',
+        'PASSWORD': 'test3',
+        'HOST': 'localhost',
+        'PORT': '',        
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
