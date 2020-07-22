cff262c0eafb0f58c64f7e07d786a0a6c7ecbf7b
mike.bochenek@gmail.com <mike.bochenek@gmail.com>
2020-07-22 16:47:23 +0200

    add basic example Scan model object

--- 01-Login/auth0login/models.py
+++ 01-Login/auth0login/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Scan(models.Model):
+    scan_type = models.IntegerField(default=0)
+    raw_text = models.CharField(max_length=200)
+    blob_id = models.IntegerField(default=0)
+    blob_url = models.CharField(max_length=200)
+    nice_filename = models.CharField(max_length=200)
+    nice_path = models.CharField(max_length=200)
+    local_path = models.CharField(max_length=200)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField()
